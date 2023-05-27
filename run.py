71be9da1907d33d79fc07487912aac6e73d3a29f
decant09 <>
2023-05-27 11:02:02 +0000

    Add function to print stock to prepare for next market

--- run.py
+++ run.py
@@ -130,7 +130,20 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
+
+def get_stock_values(data):
+    headings = SHEET.worksheet("stock").row_values(1)
+    print("Make the following numbers of sandwiches for next market:\n")
+
+    new_data = {}
+    for heading, stock_num in zip(headings, data):
+        new_data[heading] = stock_num
+    return new_data
+    
+stock_values = get_stock_values(stock_data)
+print(stock_values)
